cb6d03fcaec06da652eb91e1da4ae1477c93c0f4
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-08-07 21:16:06 +0530

    initial commit

---
+++ src/constants/app.constant.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = "Mailer Service";
+export const APP_DESCRIPTION = "Mailer Service";
---
+++ src/constants/menu.constant.ts
@@ -0,0 +1,14 @@
+import { Home } from "lucide-react";
+
+export const MenuItems = [
+  {
+    name: "Dashboard",
+    href: "/",
+    icon: Home,
+  },
+  {
+    name: "Applications",
+    href: "/applications",
+    icon: Home,
+  },
+];
